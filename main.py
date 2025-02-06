ea27029cf22f89d7ba4b6adbaf4d80bacd59333f
Mephistian-3333 <ckeller.hire@gmail.com>
2025-02-06 16:31:19 -0500

    0003 | added code to organize and sort the dictionary | bnch: main | VSCode is painful....

--- main.py
+++ main.py
@@ -31,5 +31,54 @@ def text():
             captures[char] = 1
                 # enters the 1st value when occures
     
-    print(captures)
-text()
+    # print(captures)
+
+    return captures
+
+
+values = text()
+
+
+def organize():
+    char_list = []
+        # empty list
+    
+    for char, count in values.items():
+        # refers to the defined prev fnc output    
+            # char & count capture for the pairings within the dictionary that are undefined  
+        
+        if char.isalpha():
+            # checks the 1st value to see what type it is
+
+            char_dict = {
+                "char": char,
+                "num": count
+            }
+                # compiles the detected values into a grouping
+        
+        if char.isalnum():
+            char_dict = {
+                "char": char,
+                "num": count
+            }
+
+        if char.isascii():
+            char_dict = {
+                "char": char,
+                "num": count
+            }
+        
+        char_list.append(char_dict)
+                # takes the grouping and adds it to the list in order of occurance
+    
+        # Sort the list (see the hint about sorting in the readme)
+    return char_list
+
+values = organize()
+
+def sort_on(dict):
+    return dict["num"]
+
+values.sort(reverse=True, key=sort_on)
+
+print(values)
